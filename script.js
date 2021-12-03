33b1dbfae3ad83ca30073c334516ef341259b8f0
Michael <80859948+bridgemami@users.noreply.github.com>
2021-12-03 20:35:50 +0000

    change to strict

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
      You will need to replace the contents of this JavaScript file with your own work, 
      and create any other files, if any, required for the assignment.
      When you are done, be certain to submit the assignment in both Repl.it and Canvas to be graded. */
-'use strict',
+'use strict';
 console.log("let's start");
 // define a react page component using traditional pure JS
 function FinalList(final) {
