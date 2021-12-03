a622c0b9ffa8593d3e9cce1d0054fd3f1acdab2c
Michael <80859948+bridgemami@users.noreply.github.com>
2021-12-03 16:35:23 +0000

    changes to image path

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
      You will need to replace the contents of this JavaScript file with your own work, 
      and create any other files, if any, required for the assignment.
      When you are done, be certain to submit the assignment in both Repl.it and Canvas to be graded. */
+'use strict',
+console.log("let's start");
+// define a react page component using traditional pure JS
+function FinalList(props) {
+  // return some output for the component using JSX
+}
