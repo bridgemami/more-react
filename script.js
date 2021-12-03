1856a341e233b63d8138b4297ed73f9a99642d77
Michael <80859948+bridgemami@users.noreply.github.com>
2021-12-03 19:10:37 +0000

    finished js

--- script.js
+++ script.js
@@ -5,18 +5,19 @@
 'use strict',
 console.log("let's start");
 // define a react page component using traditional pure JS
-function FinalList(props) {
+function FinalList(final) {
   // return some output for the component using JSX
   return <div className="list">
+  <h1>{final.message1}</h1>
   <ol>
   {
     // use map() to loop thru array passed in props.list, creating new element for each array value
-    props.list.map(
+    final.list.map(
       (item, index) => (
         <li key={index}>
         <a href={item.url}><img src={item.imagepath} /></a><br />
-        <p>{item.description} {item.date}<br />
-        {item.url}</p>
+        {item.description}<br /> {item.date}<br />
+        <a href={item.url} alt={item.alt}>{item.url}</a><hr /> <br />
         </li>
       )
     )
@@ -24,5 +25,19 @@ function FinalList(props) {
   </ol>
   </div>
 }
-console.log("props function";)
+console.log("props function");
 // load dataset from external JSON using jQuery
+jQuery.getJSON(
+  //getJson() takes 2 arguments: 1st is the path to file to JSON file and 2nd is a function to run with data
+  'data.json', function(jsonFromJquery) {
+    //inside this function code can run that accesses the json data
+    console.log(jsonFromJquery);
+  // call react render() to output a component into an existing html element
+    // pass properties as attributes: list passes array stored in var named data
+    // message1  passed as literal values
+    ReactDOM.render(
+      <FinalList list={jsonFromJquery} message1= "My Assignments from CS50C" />,
+      document.getElementById('myFinal')
+    );
+  }
+)
